7038b7940538eb2706c98e952f671cfc0d91726c
dylanq2021 <dylanquirk14@gmail.com>
2019-10-04 15:53:45 +0100

    added week 5 assignment

---
+++ Documents/Powershell/Week5/parsingIntellThreats.ps1
@@ -0,0 +1,41 @@
+# Storyline: Download osint files amd create iptables ruleset
+
+# Array of websites containing threat intell
+$drop_urls = @('https://rules.emergingthreats.net/blockrules/emerging-botcc.rules','https://rules.emergingthreats.net/blockrules/compromised-ips.txt')
+
+# Loop through the URLs for the rules list
+foreach ($u in $drop_urls) {
+
+    # Extract the filename
+    # Split takes a string and converts it into an array
+    $temp = $u.split("/")
+
+    $temp
+   # The last element in the array plucked off is the filename
+   $file_name = "./" + $temp[4]
+
+   # Download the rules list
+   #Invoke-WebRequest -Uri $u -OutFile $file_name
+
+}
+
+# Array containing the filename
+$input_paths = @('.\compromised-ips.txt','.\emerging-botcc.rules')
+
+# Extract the IP addresses.
+# (154.35.64.107,154.35.64.18)
+# d\ digit
+# b\ boundary
+$regex_drop = '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b'
+
+# Append the IP addresses to the temporary IP list.
+Select-String -Path $input_paths -Pattern $regex_drop -AllMatches | ForEach-Object {$_.Matches } | % { $_.value } | sort | Get-Unique -AsString | Out-File -FilePath "ips-bad.txt"
+
+# Get the IP addresses discovered, loop through and replace the beginning of the line with the IPTables syntax
+# After the IP address, add the remaining IPTables syntax and save the results to a file.
+(Get-Content -Path ".\ips-bad.txt") | % { $_ -replace "^", "iptables -A INPUT -s " -replace "$", " -j DROP" } `
+| Set-Content -Path ".\ips-bad-iptables.bash"
+
+# Task: Replace the iptables syntax above with the cisco rulset syntax. sample: access-list 1 deny host 1.1.1.2
+(Get-Content -Path ".\ips-bad.txt") | % { $_ -replace "^", "access-list 1 deny host "  } `
+| Set-Content -Path ".\ips-bad-cisco.txt"
